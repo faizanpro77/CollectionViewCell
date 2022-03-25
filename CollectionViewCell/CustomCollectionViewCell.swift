cb6626ce05bc313e0ac6f0172353eecf56bff1dc
faizanpro77 <faizyshaikh204@gmail.com>
2022-03-25 22:16:45 +0530

    Initial Commit

---
+++ CollectionViewCell/CustomCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionViewCell
+//
+//  Created by Yashom on 25/03/22.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    static let identifier = "CustomCollectionViewCell"
+    
+    private let myImageView: UIImageView =  {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "house")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let myLabel: UILabel =  {
+        let label = UILabel()
+        label.text = "Custom"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .systemRed
+        contentView.addSubview(myLabel)
+        contentView.addSubview(myImageView)
+        contentView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func  layoutSubviews() {
+        super.layoutSubviews()
+        myLabel.frame = CGRect(x: 5, y: contentView.frame.size.height-50, width: contentView.frame.size.width-10, height: 50)
+        
+        myImageView.frame = CGRect(x: 5, y: 0, width: contentView.frame.size.width-10, height: contentView.frame.size.height-50)
+        
+    }
+    
+}
---
+++ CollectionViewCell/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  CollectionViewCell
+//
+//  Created by Yashom on 25/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    
+   
+    
+    private var collectionView: UICollectionView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        layout.itemSize = CGSize(width: (view.frame.size.width/3) - 4, height: view.frame.size.width/3)
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        guard let collectionView = collectionView else {
+            return
+        }
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath)
+        return cell
+    }
+
+
+}
+
